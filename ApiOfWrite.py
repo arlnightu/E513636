7b9c979a70ecb293350da28b2c6f3471f9bf0edb
godhongkong <99140864+godhongkong@users.noreply.github.com>
2022-02-07 02:58:46 +0800

    Add files via upload

--- ApiOfWrite.py
+++ ApiOfWrite.py
@@ -39,7 +39,7 @@ def getmstoken(ms_token,appnum):
         'refresh_token': ms_token,
         'client_id':client_id,
         'client_secret':client_secret,
-        'redirect_uri':'http://localhost:53682/'
+        'redirect_uri':'http://localhost:23812/'
         }
     html = req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token',data=data,headers=headers)
     jsontxt = json.loads(html.text)
